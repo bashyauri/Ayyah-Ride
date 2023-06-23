72f3a20941151fde8edfc5054db08cf03fd4f601
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-23 20:54:52 +0100

    create cab models and migrations

---
+++ database/migrations/2023_06_23_194528_create_cab_availabities_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cab_availabities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cab_id')->constrained('cabs');
+            $table->date('date');
+            $table->boolean('available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cab_availabities');
+    }
+};
